c47e427e794783e943caffa529f1a7f3a4d43caf
prajjwaldimri <prajjwaldimri@hotmail.com>
2015-10-10 19:34:06 +0530

    Home UI

--- Android/Friend1/app/src/main/java/debug/studio/com/friend/Main2Activity.java
+++ Android/Friend1/app/src/main/java/debug/studio/com/friend/Main2Activity.java
@@ -62,6 +62,16 @@ public class Main2Activity extends AppCompatActivity
         }
         Intent in = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "7830207022"));
 
+        if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
         startActivity(in);
     }
 
