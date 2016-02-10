e4b60f9d37f2904b4144ee0cabed9e6088c13cbd
ayushbahuguna <ayush.bahuguna12@hotmail.com>
2016-02-10 14:07:27 +0530

    image view

--- Android/Friend/app/src/main/java/com/example/admin/friend/OneFragment.java
+++ Android/Friend/app/src/main/java/com/example/admin/friend/OneFragment.java
@@ -2,6 +2,7 @@ package com.example.admin.friend;
 
 import android.app.DownloadManager;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
@@ -20,6 +22,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -33,9 +36,6 @@ public class OneFragment extends android.support.v4.app.Fragment {
 
     Bitmap bmp;
     ImageView iv;
-
-    private ContentResolver contentResolver;
-
     public OneFragment() {
 
     }
@@ -43,7 +43,7 @@ public class OneFragment extends android.support.v4.app.Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        retrieve();
+
 
     }
 
@@ -51,7 +51,7 @@ public class OneFragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_one, container, false);
-        iv=(ImageView)view.findViewById(R.id.imageView);
+        iv = (ImageView) view.findViewById(R.id.imageView);
         iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,78 +60,48 @@ public class OneFragment extends android.support.v4.app.Fragment {
         });
 
 
-
-
-
         return view;
     }
 
     private void openGallery() {
-            Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
-            photoPickerIntent.setType("image/*");
-            startActivityForResult(photoPickerIntent, 1);
+        Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, 1);
 
-        }
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1) {
-            if (data != null && resultCode == RESULT_OK) {
-                Uri selectedImage = data.getData();
-                String[] filePathColumn = {MediaStore.Images.Media.DATA};
-                Cursor cursor= getContentResolver().query(selectedImage,filePathColumn,null,null,null);
-                cursor.moveToFirst();
-                int columnIndex=cursor.getColumnIndex(filePathColumn[0]);
-                String picturePath=cursor.getString(columnIndex);
-                cursor.close();
-                if (bmp!=null&&!bmp.isRecycled()){
-                    bmp=null;
-                }
-                bmp=BitmapFactory.decodeFile(picturePath);
+        try{
+            if (requestCode==RESULT_OK){
+                String path=getPathFromCameraData(data,this.getActivity());
+                Bitmap bmp=BitmapFactory.decodeFile(path);
                 iv.setImageBitmap(bmp);
 
-            } else {
-                Log.d("Status:", "Photopicker canceled");
+            }else {
+                Toast.makeText(getActivity(), "Pick your image first", Toast.LENGTH_LONG).show();
             }
-
-            storeimage();
+        }catch (Exception e){
+            e.printStackTrace();
 
         }
-
-    }
-
-    private boolean storeimage() {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        String filepaths = Environment.getExternalStorageDirectory() + "/friend2/myProfile/";
-        File image = new File(filepaths);
-        image.mkdirs();
-        try {
-            FileOutputStream fo = new FileOutputStream(filepaths);
-            if (bmp != null) {
-
-                bmp.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-            } else {
-                return false;
-            }
-
-        } catch (FileNotFoundException e1) {
-            e1.printStackTrace();
-        }
-        return true;
-    }
-    public boolean retrieve(){
-        File f = new File(android.os.Environment.getExternalStorageDirectory() + "/friend2/myProfile/");
-        Bitmap bmp = BitmapFactory.decodeFile(f.getAbsolutePath());
-        iv.setImageBitmap(bmp);
-        return  true;
-
-    }
-
-    public ContentResolver getContentResolver() {
-        return contentResolver;
-    }
 }
+
+    private String getPathFromCameraData(Intent data, Context context) {
+        Uri selectImage=data.getData();
+        String[] filepathColumn={MediaStore.Images.Media.DATA};
+        Cursor cursor=context.getContentResolver().query(selectImage, filepathColumn, null, null, null);
+        assert cursor != null;
+        cursor.moveToFirst();
+        int columnIndx=cursor.getColumnIndex(filepathColumn[0]);
+        String piturepath=cursor.getString(columnIndx);
+        cursor.close();
+        return piturepath;
+    }
+    }
+
+
 
 
 
