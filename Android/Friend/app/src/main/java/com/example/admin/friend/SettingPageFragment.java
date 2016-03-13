caff29472be9b47479d7fc2f0f81af1d561c1b37
ayushpant1 <ayushpant@hotmail.com>
2016-03-13 15:30:22 +0530

    ReminderPage and background task

--- Android/Friend/app/src/main/java/com/example/admin/friend/SettingPageFragment.java
+++ Android/Friend/app/src/main/java/com/example/admin/friend/SettingPageFragment.java
@@ -44,7 +44,7 @@ public class SettingPageFragment extends android.support.v4.app.Fragment impleme
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_settingspage, container, false);
+        view = inflater.inflate(R.layout.fragment_settingpage, container, false);
 
         toast = (Switch) view.findViewById(R.id.switch1);
         theme = (Switch) view.findViewById(R.id.switch2);
